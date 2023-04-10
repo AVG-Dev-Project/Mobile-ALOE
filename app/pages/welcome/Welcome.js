b32ae9f130fcd7d6cb8a39755c5790bd3cddc7c3
diamondrarktvo <amada.rktvo@gmail.com>
2023-04-10 20:40:02 +0300

    responsive welcome

--- app/pages/welcome/Welcome.js
+++ app/pages/welcome/Welcome.js
@@ -192,7 +192,6 @@ export default function Welcome({ navigation }) {
             style={{
                display: 'flex',
                flexDirection: 'row',
-               marginTop: 20,
             }}
          >
             <Image
