89f8933b90d9c6ca41d4368b69fa502a0e01d227
diamondrarktvo <amada.rktvo@gmail.com>
2023-05-01 23:03:18 +0300

    fix: adjust height detail_section in detailscreen.

--- app/components/detail/styles.js
+++ app/components/detail/styles.js
@@ -31,7 +31,7 @@ export const styles = StyleSheet.create({
    },
    description_section: {
       paddingHorizontal: 24,
-      height: heightDevice < 700 ? heightDevice - 230 : heightDevice - 270,
+      height: heightDevice < 700 ? heightDevice - 230 : heightDevice - 230,
       backgroundColor: Colors.white,
    },
    view_round_button_detail_article: {
