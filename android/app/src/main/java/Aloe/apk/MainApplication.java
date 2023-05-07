429b9efb906b5f91dd15df2e943980d7bb333152
JohanaMisaela <louis.sergio.rakotoarisoa@esti.mg>
2023-05-07 21:46:07 +0300

    fix: essai debug in android folder

--- android/app/src/main/java/Aloe/apk/MainApplication.java
+++ android/app/src/main/java/Aloe/apk/MainApplication.java
@@ -1,4 +1,4 @@
-package Aloe.apk;
+package aloe.apk;
 
 import android.app.Application;
 import android.content.Context;
@@ -12,7 +12,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.config.ReactFeatureFlags;
 import com.facebook.soloader.SoLoader;
-import Aloe.apk.newarchitecture.MainApplicationReactNativeHost;
+import aloe.apk.newarchitecture.MainApplicationReactNativeHost;
 
 import expo.modules.ApplicationLifecycleDispatcher;
 import expo.modules.ReactNativeHostWrapper;
@@ -88,7 +88,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("Aloe.apk.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("aloe.apk.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
